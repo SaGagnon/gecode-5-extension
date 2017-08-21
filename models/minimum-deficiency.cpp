c1eeea2ed31c2cb5cae20585e2b86f684a6f6d16
Samuel Gagnon <samuel.gagnon92@gmail.com>
2017-08-21 04:49:00 -0400

    min def

--- models/minimum-deficiency.cpp
+++ models/minimum-deficiency.cpp
@@ -10,6 +10,65 @@
 
 using namespace Gecode;
 
+template<class View>
+class aAvgSD_md : public BranchingHeuristic<View> {
+  USING_BH
+protected:
+  SharedArray<double> tot_dens;
+  SharedArray<int> prop_count;
+public:
+  aAvgSD_md(Space& home, const ViewArray<View>& x)
+    : BranchingHeuristic<View>(home,x) {
+    int size = xD.size * xD.width;
+    tot_dens.init(size);
+    prop_count.init(size);
+    for (int i=0; i<size; i++) {
+      tot_dens[i] = 0;
+      prop_count[i] = 0;
+    }
+  }
+  aAvgSD_md(Space& home, bool share, aAvgSD_md& h)
+    : BranchingHeuristic<View>(home,share,h) {
+    tot_dens.update(home,share,h.tot_dens);
+    prop_count.update(home,share,h.prop_count);
+  }
+
+  virtual Candidate getChoice(Space& home) {
+    for (int i=0; i<tot_dens.size(); i++) {
+      tot_dens[i] = 0;
+      prop_count[i] = 0;
+    }
+
+    for_every_log_entry([&](unsigned int prop_id, double slnCnt,
+                            unsigned int var_id, int val, double dens) {
+      unsigned int idx = varvalpos(xD,var_id,val);
+      tot_dens[idx] += dens;
+      prop_count[idx] += 1;
+    });
+
+    struct Best { unsigned int var_id; int val; double dens_moy;
+    } best_candidate{0,0,0};
+
+    for (unsigned int i=0; i<x.size(); i++) {
+      if (x[i].assigned()) continue;
+      if (!xD.positions.isIn(x[i].id())) continue;
+      unsigned int idx = varvalpos(xD,x[i].id(),x[i].med());
+      double dens_moy = tot_dens[idx] / (double)prop_count[idx];
+      if (dens_moy > best_candidate.dens_moy)
+        best_candidate = Best{x[i].id(),x[i].med(),dens_moy};
+    }
+
+    return Candidate{xD.positions[best_candidate.var_id],best_candidate.val};
+  }
+};
+
+
+void cbsbranch_md(Home home, const IntVarArgs& x) {
+  if (home.failed()) return;
+  ViewArray<Int::IntView> y(home,x);
+  CBSBrancher<Int::IntView,aAvgSD_md>::post(home,y);
+}
+
 class MinDef : public IntMinimizeScript {
 protected:
   std::string file_path;
@@ -71,18 +130,19 @@ public:
         v_edges[j] = edge_col[_node_edges[i][j]-1];
       distinct(*this, v_edges, opt.ipl());
       rel(*this, node_holes[i] == max(v_edges) - min(v_edges) + 1 - card);
-      rel(*this, node_holes[i] < card/16); // best = card/4
+      rel(*this, node_holes[i] < 7); // best = card/4
     }
 
     n_holes = IntVar(*this, 0, num_edges);
     rel(*this, n_holes == sum(node_holes));
-//    rel(*this, n_holes < 123);
 
-    cbsbranch(*this, edge_col, CBSBranchingHeuristic::MAX_SD);
+    cbsbranch_md(*this, edge_col);
+//    cbsbranch(*this, edge_col, CBSBranchingHeuristic::MAX_SD);
 //    branch(*this, edge_col, CBSBranchingHeuristic::MAX_SD);
-    branch(*this, edge_col, INT_VAR_SIZE_MIN(), INT_VAL_SPLIT_MIN());
+    branch(*this, edge_col, INT_VAR_SIZE_MIN(), INT_VAL_MIN());
+//    branch(*this, edge_col, INT_VAR, INT_VAL_MIN());
 //    branch(*this, edge_col, INT_VAR_AFC_MAX(opt.decay()), INT_VAL_SPLIT_MIN());
-//    branch(*this, edge_col, INT_VAR_ACTIVITY_SIZE_MAX(opt.decay()), INT_VAL_MIN());
+//    branch(*this, edge_col, INT_VAR_REGRET_MIN_MAX(), INT_VAL_MIN());
   }
 
   MinDef(bool share, MinDef& s)
@@ -108,12 +168,13 @@ public:
 
 int main(int argc, char *argv[]) {
   InstanceOptions opt("Minimum Deficiency");
-  opt.ipl(IPL_BND);
+  opt.ipl(IPL_DOM);
 //  opt.c_d(20);
   opt.solutions(0);
 
 //  DSJC1000.5.col.gecode
 //  DSJC125.5.col.gecode
+
 //  DSJC500.5.col.gecode
 //  maison_5_7.col.gecode
 //  queen5_5.col.gecode
