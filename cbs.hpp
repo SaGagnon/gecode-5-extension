55f6d96b11c700e4da68a703372022d701a0c5ce
Samuel Gagnon <samuel.gagnon92@gmail.com>
2017-09-24 18:12:54 -0400

    Refactoring

--- cbs.hpp
+++ cbs.hpp
@@ -1,19 +1,10 @@
 #ifndef __CBS_HPP__
 #define __CBS_HPP__
 
-#include <gecode/int.hh>
-#include <ext/hash_map>
-#include <ext/hash_set>
-#include <cstring>
-#include <tuple>
-#include <functional>
-#include <map>
+#include <iostream>
 #include <unordered_map>
 
-#include <fstream>
-#include <iostream>
-#include <set>
-
+#include <gecode/int.hh>
 
 #ifdef SQL
 #include <sql-interface.hh>
@@ -27,6 +18,9 @@ using Val     = int;
 using Dens    = double;
 using SlnCnt  = double;
 
+/**
+ * \brief Density records for a propagator
+ */
 class PropInfo {
 public:
   struct Record {
@@ -36,51 +30,37 @@ public:
   };
 private:
   struct Records {
-    // During the insertion in *records, we need to keep track of the current
-    // position. At the end, curr_pos == records_size
+    /// Position for inserting the next record
     unsigned int pos{0};
-
-    // Density information for every (var,val) pair
+    /// List of records
     Record *x{nullptr};
-
-    // Total number of (var,val) pair the propagator shares with the brancher
-    // and needs to communicate.
+    /// Number of records allocated
     size_t size{0};
   } records;
 
   SlnCnt slnCount{0};
-
-  // Sum of all domains of non assigned variable in the propagator. We need this
-  // information along with records.size because we can't rely only on
-  // records.size to know how many changes occured in the propagator (if we
-  // want to trigger recomputation with a changes pourcentage cutoff).
+  /// Sum of all domains of non assigned variable
   size_t domAggr{0};
-
 public:
   PropInfo() = default;
-
   PropInfo(Space& home, size_t domAggr0, size_t domAggrB)
     : domAggr(domAggr0) {
     records.x = home.alloc<Record>(domAggrB);
     records.size = domAggrB;
   }
-
   PropInfo(Space& home, const PropInfo& o)
     : domAggr(o.domAggr), records(o.records), slnCount(o.slnCount) {
     records.x = home.alloc<Record>(records.size);
     memcpy(records.x, o.records.x, records.pos * sizeof(Record));
   }
-
   Record& operator[](unsigned int i) {
     assert(records.x != nullptr);
     assert(i < records.size);
     return records.x[i];
   }
-
   Record operator[](unsigned int i) const {
     return const_cast<PropInfo*>(this)->operator[](i);
   }
-
   size_t getDomAggr() const { return domAggr; }
   size_t getDomAggrB() const { return records.size; }
   unsigned int getPosRec() const { return records.pos; }
@@ -90,7 +70,6 @@ public:
     assert(cnt > 1);
     slnCount = cnt;
   }
-
   void insert_record(const Record&& r) {
     assert(records.x != nullptr);
     assert(r.dens>0 && r.dens<1);
@@ -98,7 +77,6 @@ public:
     records.pos++;
     assert(records.size >= records.pos);
   }
-
   void reuse_space(size_t s) {
     // We discard the previous entries by setting the count to 0 (we
     // thus reuse previous allocated memory. The number of records can't
@@ -108,31 +86,32 @@ public:
   }
 };
 
+typedef std::unordered_map<PropId, PropInfo,
+  std::hash<PropId>, std::equal_to<PropId>,
+  Gecode::space_allocator<PropInfo> > LogProp;
 
-typedef __gnu_cxx::hash_map<PropId, PropInfo,
-  __gnu_cxx::hash<PropId>, __gnu_cxx::equal_to<PropId>,
-  Gecode::space_allocator<PropInfo> >
-  LogProp;
-
+/**
+ * \brief Maps variables ids to indexes
+ */
 class VarIdToPos : public SharedHandle {
 protected:
   class VarIdToPosO : public SharedHandle::Object {
   public:
-    typedef __gnu_cxx::hash_map<unsigned int, unsigned int> HashMap;
+    typedef std::unordered_map<unsigned int, unsigned int> HashMap;
     HashMap hash_map;
   public:
-    VarIdToPosO(void) {}
+    VarIdToPosO() = default;
     VarIdToPosO(const VarIdToPosO& o)
       : hash_map(o.hash_map) {}
-    virtual Object* copy(void) const {
+    Object* copy() const override {
       return new VarIdToPosO(*this);
     }
-    virtual ~VarIdToPosO(void) {}
+    ~VarIdToPosO() override = default;
   };
 public:
-  VarIdToPos(void) {}
-  void init(void) {
-    assert(object() == NULL);
+  VarIdToPos() = default;
+  void init() {
+    assert(object() == nullptr);
     object(new VarIdToPosO());
   }
   bool isIn(unsigned int i) const {
@@ -148,9 +127,9 @@ public:
   }
 };
 
-class CBSPos : public SharedHandle {
+class VarInBrancher : public SharedHandle {
 protected:
-  class CBSPosO : public SharedHandle::Object {
+  class VarInBrancherO : public SharedHandle::Object {
   public:
     class BoolArray : public SolnDistributionSize {
     private:
@@ -189,7 +168,7 @@ protected:
         std::cout << "CECI DOIT APPARAITRE" << std::endl;
         heap.free(inBrancher, size());
       }
-      virtual bool varInBrancher(VarId var_id) const {
+      bool varInBrancher(VarId var_id) const override {
         if (var_id-min_id < 0) return false;
         if (var_id-min_id >= size()) return false;
         return get(var_id);
@@ -197,21 +176,20 @@ protected:
     };
     BoolArray bool_array;
   public:
-//    CBSPosO() = default;
     template<class View>
-    explicit CBSPosO(const ViewArray<View>& x) : bool_array(x) {}
-    CBSPosO(const CBSPosO& o) : bool_array(o.bool_array) {}
-    virtual Object* copy() const { return new CBSPosO(*this); }
-    ~CBSPosO() override = default;
+    explicit VarInBrancherO(const ViewArray<View>& x) : bool_array(x) {}
+    VarInBrancherO(const VarInBrancherO& o) : bool_array(o.bool_array) {}
+    Object* copy() const override { return new VarInBrancherO(*this); }
+    ~VarInBrancherO() override = default;
   };
 public:
   template<class View>
-  explicit CBSPos(const ViewArray<View>& x) {
+  explicit VarInBrancher(const ViewArray<View>& x) {
     assert(object() == nullptr);
-    object(new CBSPosO(x));
+    object(new VarInBrancherO(x));
   }
-  CBSPosO::BoolArray& getObject() const {
-    return static_cast<CBSPosO*>(object())->bool_array;
+  VarInBrancherO::BoolArray& getObject() const {
+    return static_cast<VarInBrancherO*>(object())->bool_array;
   }
 };
 
@@ -227,13 +205,12 @@ protected:
   const double recomputation_ratio;
   ViewArray<View> x;
   VarIdToPos varpos;
-  CBSPos cbs_pos;
-//  BranchingHeur<View> heur;
+  VarInBrancher varInBrancher;
   LogProp logProp;
 public:
   CBSBrancher(Home home, ViewArray<View>& x0, double recomputation_ratio0)
     : Brancher(home), recomputation_ratio(recomputation_ratio0),
-      x(x0), cbs_pos(x0),
+      x(x0), varInBrancher(x0),
       logProp(LogProp::size_type(), LogProp::hasher(),
                 LogProp::key_equal(), LogProp::allocator_type(home)) {
     // Because we must call the destructor of aAvgSD
@@ -252,7 +229,7 @@ public:
     assert(recomputation_ratio > 0 && recomputation_ratio <= 1);
     (void) new (home) Derived(home,x,recomputation_ratio);
   }
-  virtual size_t dispose(Space& home) {
+  size_t dispose(Space& home) override {
     home.ignore(*this, AP_DISPOSE);
     // ~aAvgSD() calls ~SharedHashMap() which calls ~SharedHashMapObject() to
     // deallocate the hash map when the refcount of SharedHashMapObject is 0
@@ -263,7 +240,7 @@ public:
   }
   CBSBrancher(Space& home, bool share, CBSBrancher& b)
     : Brancher(home,share,b), recomputation_ratio(b.recomputation_ratio),
-      cbs_pos(b.cbs_pos),
+      varInBrancher(b.varInBrancher),
       logProp(b.logProp.begin(), b.logProp.end(),
                  LogProp::size_type(), LogProp::hasher(),
                  LogProp::key_equal(), LogProp::allocator_type(home)) {
@@ -279,18 +256,17 @@ public:
     for (auto& elem : logProp)
       elem.second = PropInfo(home, b.logProp[elem.first]);
   }
-  virtual Brancher* copy(Space& home, bool share) {
-//    CBSBrancher *ret = home.alloc<CBSBrancher>(1);
+  Brancher* copy(Space& home, bool share) override {
     return new (home) Derived(home,share,*this);
   }
-  virtual bool status(const Space& home) const {
-    Space& h = const_cast<Space&>(home);
+  bool status(const Space& home) const override {
+    auto& h = const_cast<Space&>(home);
     for (Propagators p(h, PropagatorGroup::all); p(); ++p) {
       // Sum of domains of all variable in propagator
       unsigned int domAggr;
       // Same, but for variables that are also in this brancher.
       unsigned int domAggrB;
-      p.propagator().slndistsize(&cbs_pos.getObject(), domAggr, domAggrB);
+      p.propagator().slndistsize(&varInBrancher.getObject(), domAggr, domAggrB);
       // If there's still a propagator that has an unassigned variable that is
       // also in the brancher, we tell our brancher has still work to do.
       if (domAggrB > 0)
@@ -298,16 +274,15 @@ public:
     }
     return false;
   }
-
   virtual Candidate getChoice(Space& home) = 0;
 
-  virtual bool compute(VarId var_id) const {
+  bool compute(VarId var_id) const override {
     return true;
   }
   // Method used by all propagators for communicating calculated densities for
   // each of its (variable,value) pair.
-  virtual void setMarginalDistribution(PropId prop_id, VarId var_id, Val val,
-                                       Dens density) {
+  void setMarginalDistribution(PropId prop_id, VarId var_id, Val val,
+                                       Dens density) override {
     assert(var_id != 0);
     if (!varpos.isIn(var_id)) return;
     assert(!x[varpos[var_id]].assigned());
@@ -316,22 +291,19 @@ public:
 
     logProp[prop_id].insert_record(PropInfo::Record{var_id, val, density});
   }
-  virtual void setSupportSize(PropId prop_id, SlnCnt count) {
+  void setSupportSize(PropId prop_id, SlnCnt count) override {
     logProp[prop_id].setSlnCnt(count);
   }
-
-
-
-  virtual const Choice* choice(Space& home) {
+  const Choice* choice(Space& home) override {
     // Active propagators and the size we need for their log.
     // We considere a propagator as "active" only if
     // - it supports slndist()
     // - it has unassigned variables that are also in the brancher
     struct Psize { size_t domAggr; size_t domAggrB; };
-    __gnu_cxx::hash_map<PropId, Psize > activeProps;
+    std::unordered_map<PropId, Psize > activeProps;
     for (Propagators p(home, PropagatorGroup::all); p(); ++p) {
       unsigned int domAggr, domAggrB;
-      p.propagator().slndistsize(&cbs_pos.getObject(), domAggr, domAggrB);
+      p.propagator().slndistsize(&varInBrancher.getObject(), domAggr, domAggrB);
       if (domAggrB != 0) {
         activeProps[p.propagator().id()] = {domAggr, domAggrB};
       }
@@ -368,11 +340,9 @@ public:
       }
 
       if (!in_log || changed) {
-//        p.propagator().slndist(home,&heur,SolnDistribution::MAX_PER_PROP);
         p.propagator().slndist(home,&*this);
       }
     }
-
 
     // We find the choice.
     Candidate c = getChoice(home);
@@ -381,24 +351,22 @@ public:
 
     return new PosValChoice<int>(*this,2,c.idx,c.val);
   }
-  virtual const Choice* choice(const Space&, Archive& e) {
+  const Choice* choice(const Space&, Archive& e) override {
     int pos, val;
     e >> pos >> val;
     return new PosValChoice<int>(*this,2,pos,val);
   }
-  // commit
-  virtual ExecStatus commit(Space& home, const Choice& c, unsigned int a) {
-    const PosValChoice<int>& pvc = static_cast<const PosValChoice<int>&>(c);
+  ExecStatus commit(Space& home, const Choice& c, unsigned int a) override {
+    const auto& pvc = static_cast<const PosValChoice<int>&>(c);
     int pos=pvc.pos().pos, val=pvc.val();
     if (a == 0)
       return me_failed(x[pos].eq(home,val)) ? ES_FAILED : ES_OK;
     else
       return me_failed(x[pos].nq(home,val)) ? ES_FAILED : ES_OK;
   }
-  // print
-  virtual void print(const Space& home, const Choice& c, unsigned int a,
-                     std::ostream& o) const {
-    const PosValChoice<int>& pvc = static_cast<const PosValChoice<int>&>(c);
+  void print(const Space& home, const Choice& c, unsigned int a,
+                     std::ostream& o) const override {
+    const auto& pvc = static_cast<const PosValChoice<int>&>(c);
     int pos=pvc.pos().pos, val=pvc.val();
     if (a == 0)
       o << "x[" << pos << "] = " << val;
@@ -454,7 +422,6 @@ enum CBSBranchingHeuristic {
   AI
 };
 
-
 template<class View, class T>
 void _cbsbranch(Home home, const T& x, CBSBranchingHeuristic s) {
   if (home.failed()) return;
@@ -464,18 +431,16 @@ void _cbsbranch(Home home, const T& x, CBSBranchingHeuristic s) {
       MAXSD<View>::post(home,y,1);
       break;
     default:
-      GECODE_NEVER;
+      assert(false);
   }
 }
 
-void cbsbranch(Home home, const IntVarArgs& x, CBSBranchingHeuristic s) {
+void cbsbranch(const Home& home, const IntVarArgs& x, CBSBranchingHeuristic s) {
   _cbsbranch<Int::IntView,IntVarArgs>(home,x,s);
 }
 
-void cbsbranch(Home home, const BoolVarArgs& x, CBSBranchingHeuristic s) {
+void cbsbranch(const Home& home, const BoolVarArgs& x, CBSBranchingHeuristic s) {
   _cbsbranch<Int::BoolView,BoolVarArgs>(home,x,s);
 }
-
-
 
 #endif //__CBS_HPP__
